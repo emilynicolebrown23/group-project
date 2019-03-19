3e0413062de1b334a2086512d9d708d6dfa06081
Jessica Beeli <jmbeeli@gmail.com>
2019-03-19 17:04:52 -0600

    initial react app

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import Nav from './components/Nav'
+import Contact from './components/Contact'
+import Involved from './components/Involved'
+import Prevention from './components/Prevention'
+import Resources from './components/Resources'
+import Landing from './components/Landing'
+import Home from './components/Home'
+import './style.css'
+
+class App extends Component {
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         user: "Joe"
+    //     }
+    // }
+
+
+    render(){
+        return (
+            <div className="app-container">
+                <Nav/>
+                <Switch>
+                    <Route exact path="/" component= { Home } />
+                    <Route path="/landing" component= { Landing } />
+                    <Route path="/contact" component= { Contact } />
+                    <Route path="/resources" component= { Resources } />
+                    <Route path="/prevention" component={ Prevention } />
+                    <Route path="/involved" component={ Involved } /> 
+                </Switch>
+
+            </div>
+        )
+    }
+}
+
+
+export default withRouter(App)
---
+++ client/src/context/ShelterProvider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+const ShelterContext = React.createContext()
+
+class ShelterProvider extends Component {
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+}
