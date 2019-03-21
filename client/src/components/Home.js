2b912a62928121d3b6da5774368f942201139cff
Jessica Beeli <jmbeeli@gmail.com>
2019-03-20 18:00:04 -0600

    things are working yay

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Searchbar from '../Searchbar'
+import SearchList from './SearchList'
 
 const Home = () => {
     return (
         <div className="home-container">
                
             <Searchbar/>
+            <SearchList/>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import ShelterProvider from './context/ShelterProvider'
 
 ReactDOM.render(
+    
     <BrowserRouter>
-        <App />
-    </BrowserRouter>
+        <ShelterProvider>
+            <App />
+        </ShelterProvider>
+     </BrowserRouter>
 , document.getElementById('root'))
